79294deb3e08c99c04f8ef0ae75b0a8464b64795
Yuto Tachibana <tachibanayu24@gmail.com>
2018-08-22 00:04:25 +0900

    bug fix

--- db/migrate/20180815074613_remove_twitter_account_name_from_users.rb
+++ db/migrate/20180815074613_remove_twitter_account_name_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveTwitterAccountNameFromUsers < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :twitter_account_name, :string
+    remove_column :users, :twitter_account_name
   end
 end
